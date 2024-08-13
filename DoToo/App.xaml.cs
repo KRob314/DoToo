9251047d07dd7f4f6ffd8a05020e0b2a474be2f8
KRob314 <krob636@gmail.com>
2024-08-12 19:52:38 -0700

    Add project files.

---
+++ DoToo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DoToo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DoToo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DoToo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
