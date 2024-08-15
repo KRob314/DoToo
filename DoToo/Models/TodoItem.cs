cd2803af2ebe0c2cafe44350e8f89f9d4e039e23
KRob314 <krob636@gmail.com>
2024-08-14 21:34:34 -0700

    Setup repo and SQLite

---
+++ DoToo/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace DoToo.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Due { get; set; }
+    }
+}
---
+++ DoToo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using DoToo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoToo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdated;
+        Task<List<TodoItem>> GetItemsAsync();
+        Task AddItemAsync(TodoItem item);
+        Task UpdateItemAsync(TodoItem item);
+        Task AddOrUpdateAsync(TodoItem item);
+    }
+}
